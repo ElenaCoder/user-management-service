c0fe6abc540196f7da72afbc65310a18fe481d2d
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-09-10 13:07:33 +0300

    feat: confirm that the two entered passwords match

--- authController.js
+++ authController.js
@@ -6,6 +6,12 @@ const showRegistrationForm = (c) => c.html(eta.render("registration.eta"));
 
 const registerUser = async (c) => {
     const body = await c.req.parseBody();
+
+    // Confirming that the two passwords match
+    if (body.password !== body.verification) {
+        return c.text("The passwords you entered don’t match");
+    }
+
     return c.text(JSON.stringify(body));
   };
 
